6ddbddefb4dc1cd548fe642d0b25a4557da8d10d
carpediemkairos <carpediem.kairos1@gmail.com>
2026-08-07 00:23:37 +0600

    Add Cloudflare Web Analytics (real-user performance monitoring)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://carpe-diem.pages.dev',
+  site: 'https://carpe-diem1.pages.dev',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     mdx()
